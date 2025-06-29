5a2a8f0541da129c470cbfb7844b6c7c54aa0f80
yujaw <yujawmanandhar@gmail.com>
2025-06-29 12:58:55 +0545

    Outreach and About Section Changed

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,4 @@
 import { Fragment } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { EffectCoverflow, Autoplay } from 'swiper/modules'
 
 const About = () => {
     return (
@@ -14,66 +12,12 @@ const About = () => {
                             </div>
 
                             <div className="content">
-                                <span>I'm Yujaw, a computer engineering student with a deep passion for technology, especially in the realm of penetration testing. As a student, I'm eager to delve deeper into this field and explore its intricacies further.</span>
-
-                                <div className="technologies">
-                                    <div className="title">Technologies</div>
-                                    <div className="container">
-                                        <Swiper
-                                            spaceBetween={50}
-                                            slidesPerView={window.innerWidth < 768 ? 3 : 5}
-                                            loop={true}
-                                            modules={[EffectCoverflow, Autoplay]}
-                                            className='swiper-container'
-                                            effect='coverflow'
-                                            coverflowEffect={{
-                                                rotate: 20,
-                                                stretch: 0,
-                                                depth: 100,
-                                                modifier: 1,
-                                                slideShadows: true,
-                                            }}
-                                            autoplay={{
-                                                delay: 2500,
-                                                disableOnInteraction: false
-                                            }}
-                                        >
-                                            <SwiperSlide>
-                                                <img src="./images/technology/js.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/html.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/node.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/php.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/python.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/mongodb.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/mysql.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/c.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/sass.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/react.png" alt="technology" />
-                                            </SwiperSlide>
-                                            <SwiperSlide>
-                                                <img src="./images/technology/css.png" alt="technology" />
-                                            </SwiperSlide>
-                                        </Swiper>
-                                    </div>
-                                </div>
+                                <span>  
+                                    I am a computer engineering student with a strong passion for hardware and a deep curiosity about how things work at a low level. I enjoy exploring circuits, embedded systems, and building projects that integrate both hardware and software. While I write clean and efficient code, what truly fascinates me is making hardware and software communicate seamlessly.
+                                    <br />
+                                    <br />
+                                    Alongside my hardware interests, I also work on full-stack web development and software projects to broaden my skills. I’m always eager to take on hands-on challenges that deepen my understanding of technology from the inside out, whether it’s wiring a microcontroller or optimizing a program.
+                                </span>
                             </div>
                         </div>
                         <div className="right">
--- src/components/Outreach/index.jsx
+++ src/components/Outreach/index.jsx
@@ -16,18 +16,6 @@ const Outreach = () => {
                     <div className="content">
                         <div className="item">
                             <div className="text">
-                                <div className="title">Nepal Scouts</div>
-                                <div className="sub_title">Member</div>
-                                <div className="sub">As a member of Nepal Scouts, I participated in community service, leadership training, and teamwork-focused activities. Through camps and volunteer projects, I also developed basic survival skills, including knot tying and first aid, while contributing to public awareness and local outreach initiatives.</div>
-                            </div>
-                            {/* <div className="image-container">
-                                <div className="image">
-                                    <img src="./images/outreach/scout.png" alt="outreach" />
-                                </div>
-                            </div> */}
-                        </div>
-                        <div className="item">
-                            <div className="text">
                                 <div className="title">WSU Hackathon 2025</div>
                                 <div className="sub_title">Participant </div>
                                 <div className="sub">Participated in Wright State University Hackathon 2025, working on a basic web app as part of a team that used modern development and deployment practices. The experience helped me improve my collaboration and quick development skills.</div>
@@ -44,11 +32,13 @@ const Outreach = () => {
                                 <div className="sub_title">Team Runner-Up</div>
                                 <div className="sub">Our team secured the runner-up position in the Uniglobe Intra-School Basketball Tournament, demonstrating strong teamwork, dedication, and sportsmanship.</div>
                             </div>
-                            {/* <div className="image-container">
-                                <div className="image">
-                                    <img src="./images/outreach/scout.png" alt="outreach" />
-                                </div>
-                            </div> */}
+                        </div>
+                        <div className="item">
+                            <div className="text">
+                                <div className="title">Nepal Scouts</div>
+                                <div className="sub_title">Member</div>
+                                <div className="sub">As a member of Nepal Scouts, I participated in community service, leadership training, and teamwork-focused activities. Through camps and volunteer projects, I also developed basic survival skills, including knot tying and first aid, while contributing to public awareness and local outreach initiatives.</div>
+                            </div>
                         </div>
                     </div>
                 </div>
